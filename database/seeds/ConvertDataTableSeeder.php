7584ba725b54b1acaf0a86b2a8221be2996a8926
yaser <yaser.darzi@gmail.com>
2018-09-15 14:37:25 +0430

    convert data to table words

--- database/seeds/ConvertDataTableSeeder.php
+++ database/seeds/ConvertDataTableSeeder.php
@@ -14,13 +14,14 @@ class ConvertDataTableSeeder extends Seeder
     {
         $words = DB::table("504")->get();
         foreach ($words as $key => $value) {
-            \App\Words::create([
-                "pronunciation" => $value->COL2,
-                "examples" => $value->COL3,
-                "word" => $value->COL4,
-                "definition" => $value->COL5,
-                "persian" => $value->COL6
-            ]);
+            if ($key != 0)
+                \App\Words::create([
+                    "pronunciation" => $value->COL2,
+                    "examples" => $value->COL3,
+                    "word" => $value->COL4,
+                    "definition" => $value->COL5,
+                    "persian" => $value->COL6
+                ]);
             echo $key . "\n";
         }
         echo "finish\n";
